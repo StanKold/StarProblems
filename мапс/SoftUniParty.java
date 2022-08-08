ea0cb958b5372c4f940f7c0ff00e5b912a9ef325
StanKold <103124654+StanKold@users.noreply.github.com>
2022-08-08 23:00:52 +0300

    Create SoftUniParty.java

---
+++ мапс/SoftUniParty.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SoftUniParty {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Set<String> guestList = new TreeSet<>();
+        String input = scanner.nextLine();
+        while (!input.equals("PARTY")) {
+            guestList.add(input);
+
+            input = scanner.nextLine();
+        }
+        input = scanner.nextLine();
+        while (!input.equals("END")) {
+            guestList.remove(input);
+
+            input = scanner.nextLine();
+        }
+        System.out.println(guestList.size());
+        for (String s : guestList) {
+            System.out.println(s);
+        }
+
+    }
+}
